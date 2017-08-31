d8f812f01293986275708c7263d94f9ff234caaa
Askirkela <ldebene@gmail.com>
2017-08-31 17:50:11 +0200

    Fixed weatherService

--- app/app.js
+++ app/app.js
@@ -21,39 +21,25 @@ angular.module('myApp', [
         var url = 'https://api.openweathermap.org/data/2.5/weather?q=';
         var apiKeyString = '&APPID=dc393667966650c4415b7190103fcc7d';
         var metricString = '&units=metric';
-        var widgets = [{
-                name: 'Paris',
-                country: 'FR',
-                temp: '25°C',
-                weatherIcon: 'wi wi-owm-503',
-                desc: 'Cloudy'
-            },
-            {
-                name: 'Rouen',
-                country: 'FR',
-                temp: '22°C',
-                weatherIcon: 'wi wi-owm-503',
-                desc: 'Rainy'
-            }
-        ];
+        var widgets = [];
         this.create = function(city) {
             $http.get(url + city + apiKeyString + metricString)
                 .then(function(res) {
                     var w = {
-                        name: res.json().name,
-                        country: res.json().sys.country,
-                        temp: res.json().main.temp,
-                        weatherIcon: 'wi wi-owm-' + res.json().weather[0].id,
-                        desc: res.json().weather[0].description
+                        id: widgets.length == 0 ? 0 : widgets.length,
+                        name: res.data.name,
+                        country: res.data.sys.country,
+                        temp: Math.round(res.data.main.temp) + '°C',
+                        weatherIcon: 'wi wi-owm-' + res.data.weather[0].id,
+                        desc: res.data.weather[0].description
                     };
                     widgets.push(w);
                 });
         };
-        this.delete = function(city) {
-            widgets = widgets.filter(function(w) { return w.name !== city; });
+        this.delete = function(id) {
+            widgets = widgets.filter(function(w) { return w.id !== id; });
         };
         this.getWeather = function() {
-            console.log(JSON.stringify(widgets));
             return widgets;
         };
     }]);
